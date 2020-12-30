2786f4b6580abad23ae026044b04e5ad7dc1ef0f
GerardoFloresSan <gerardo.gabriel.flores@gmail.com>
2020-12-30 08:01:48 -0500

    Fix's

--- app/src/main/java/com/example/myfirstapp/ui/activity/security/EndOrderAppActivity.kt
+++ app/src/main/java/com/example/myfirstapp/ui/activity/security/EndOrderAppActivity.kt
@@ -1,19 +1,15 @@
 package com.example.myfirstapp.ui.activity.security
 
 import android.os.SystemClock
-import android.util.Log
 import android.view.View
 import com.example.myfirstapp.R
 import com.example.myfirstapp.data.response.CloseCartResponse
 import com.example.myfirstapp.ui.application.RipleyApplication
-import com.example.myfirstapp.ui.base.PdfBaseActivity
-import com.example.myfirstapp.ui.base.RipleyBaseActivity
 import com.example.myfirstapp.ui.base.ScanBlueToothBaseActivity
 import com.example.myfirstapp.utils.PapersManager
-import com.example.myfirstapp.utils.ProcessBitmap
+import com.example.myfirstapp.utils.setColorBackground
 import com.example.myfirstapp.utils.startActivityE
-import kotlinx.android.synthetic.main.activity_end_order.*
-import kotlinx.android.synthetic.main.activity_welcome_seguridad.*
+import kotlinx.android.synthetic.main.activity_end_order_app_activity.*
 
 class EndOrderAppActivity : ScanBlueToothBaseActivity() {
 
@@ -21,13 +17,12 @@ class EndOrderAppActivity : ScanBlueToothBaseActivity() {
 
     private var mLastClickTime: Long = 0
 
-    override fun getView(): Int = R.layout.activity_end_order
-
-    /*private lateinit var list: MutableList<CloseCartResponse.ClientVoucher>*/
+    override fun getView(): Int = R.layout.activity_end_order_app_activity
 
     override fun onCreate() {
-        closeCart = intent.getSerializableExtra("extra0") as CloseCartResponse
+        super.onCreate()
 
+        closeCart = intent.getSerializableExtra("extra0") as CloseCartResponse
         initTextPaint()
         initBlueToothScanPrint()
 
@@ -36,32 +31,46 @@ class EndOrderAppActivity : ScanBlueToothBaseActivity() {
             startActivityE(WelcomeSecurityActivity::class.java)
         }
 
-        if(PapersManager.macPrint.isEmpty()) {
+        //VALIDACIONES
+        if (PapersManager.macPrint.isEmpty()) {
             btn_print.visibility = View.GONE
-            btn_print_2.visibility = View.VISIBLE
+            btn_config_blue.visibility = View.VISIBLE
+            btn_close_all.isEnabled = false
+            btn_close_all.isClickable = false
+            btn_close_all.isFocusable = false
         } else {
+            val validation = (PapersManager.macPrint.isEmpty())
+            btn_close_all.isEnabled = validation
+            btn_close_all.isClickable = validation
+            btn_close_all.isFocusable = validation
+            btn_close_all.setColorBackground(
+                validation,
+                this,
+                R.color.colorPrimary,
+                R.color.colorPrimaryOpa
+            )
             btn_print.visibility = View.VISIBLE
-            btn_print_2.visibility = View.GONE
+            btn_config_blue.visibility = View.GONE
         }
 
         btn_print.setOnClickListener {
             initPrint(PapersManager.macPrint, closeCart.clientVoucher, false)
         }
 
-        btn_print_2.setOnClickListener {
+        text_config.setOnClickListener {
             if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) {
                 return@setOnClickListener
             }
             mLastClickTime = SystemClock.elapsedRealtime()
 
-            showBlueToothDevice{ action ->
-                btn_print_2.visibility = if(action) View.GONE else View.VISIBLE
-                btn_print.visibility = if(action) View.VISIBLE else View.GONE
+            showBlueToothDevice { action ->
+                btn_config_blue.visibility = if (action) View.GONE else View.VISIBLE
+                btn_print.visibility = if (action) View.VISIBLE else View.GONE
             }
         }
-        super.onCreate()
-    }
 
+
+    }
 
 
     override fun onBackPressed() {
