1f43543b4d806329ebf11f8cdd4ea0887b01b878
GerardoFloresSan <gerardo.gabriel.flores@gmail.com>
2021-06-24 20:45:34 -0500

    .

--- app/src/main/java/com/example/myfirstapp/ui/activity/security/ListDetailActivity.kt
+++ app/src/main/java/com/example/myfirstapp/ui/activity/security/ListDetailActivity.kt
@@ -1,14 +1,21 @@
 package com.example.myfirstapp.ui.activity.security
 
 import com.example.myfirstapp.R
+import com.example.myfirstapp.data.request.GetStateByQrRequest
 import com.example.myfirstapp.data.response.SalesGetByResponse
+import com.example.myfirstapp.presenter.SalesPresenter
 import com.example.myfirstapp.ui.adapter.SalesAdapter
 import com.example.myfirstapp.ui.base.RipleyBaseActivity
+import com.example.myfirstapp.utils.PapersManager
 import com.example.myfirstapp.utils.startActivityE
 import kotlinx.android.synthetic.main.activity_list_detail.*
+import java.io.Serializable
+import javax.inject.Inject
 
-class ListDetailActivity : RipleyBaseActivity() {
+class ListDetailActivity : RipleyBaseActivity(), SalesPresenter.View {
 
+    @Inject
+    lateinit var salesPresenter: SalesPresenter
     lateinit var listSales : ArrayList<SalesGetByResponse>
     private var adapter: SalesAdapter? = null
 
@@ -16,6 +23,8 @@ class ListDetailActivity : RipleyBaseActivity() {
 
     @Suppress("UNCHECKED_CAST")
     override fun onCreate() {
+        component.inject(this)
+        salesPresenter.attachView(this)
         listSales = intent.getSerializableExtra("extra0") as ArrayList<SalesGetByResponse>
         btn_back.setOnClickListener {
             finish()
@@ -23,7 +32,15 @@ class ListDetailActivity : RipleyBaseActivity() {
         adapter = SalesAdapter { status, sale ->
             when (status) {
                 0 -> {
-                    startActivityE(DetailShopActivity::class.java, sale)
+                    if(sale.hashQr.isNullOrEmpty()) {
+                        startActivityE(DetailShopActivity::class.java, sale)
+                    } else {
+                        salesPresenter.getUserByQr(GetStateByQrRequest().apply {
+                            this.hashQr = sale.hashQr
+                            this.username = PapersManager.username
+                            this.token = PapersManager.loginAccess.token
+                        })
+                    }
                 }
                 else -> {}
             }
@@ -33,5 +50,10 @@ class ListDetailActivity : RipleyBaseActivity() {
         recycler.adapter = adapter
         adapter!!.data = listSales
         super.onCreate()
+    }
+
+    override fun salesSuccessPresenter(status: Int, vararg args: Serializable) {
+        val response = args[0] as SalesGetByResponse
+        startActivityE(DetailShopActivity::class.java, response)
     }
 }
